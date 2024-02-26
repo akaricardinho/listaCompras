4201d710466296c0ca676080c58ae34b09c19018
Usuario <Usuario@CPD03-M05>
2024-02-26 10:55:46 -0300

    Adicionar arquivos de projeto.

---
+++ listaCompras/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace listaCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ listaCompras/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace listaCompras
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
