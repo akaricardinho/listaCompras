db26978f45037259833621a44d1e79f118f43a0b
Usuario <Usuario@CPD03-M05>
2024-02-26 11:35:40 -0300

    Código inserido em SQLiteDataBaseHelper.cs

--- listaCompras/Helpers/SQLiteDataBaseHelper.cs
+++ listaCompras/Helpers/SQLiteDataBaseHelper.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using listaCompras.Models;
+using SQLite;
 
 namespace listaCompras.Helpers
 {
-    internal class SQLiteDataBaseHelper
+    public class SQLiteDataBaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDataBaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> Insert (Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
     }
 }
