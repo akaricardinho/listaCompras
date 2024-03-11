47e5151f5460228cbb782167c43a54b357b9761f
Usuario <Usuario@CPD03-M05>
2024-03-11 12:21:38 -0300

    Atualização da pasta view

---
+++ listaCompras/Views/NovoProduto.xaml.cs
@@ -0,0 +1,28 @@
+using listaCompras.Models;
+
+namespace listaCompras.Views;
+
+public partial class NovoProduto : ContentPage
+{
+	public NovoProduto()
+	{
+		InitializeComponent();
+	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto();
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text),
+			};
+		}
+		catch (Exception ex) 
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
+}
