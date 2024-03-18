e6f071fb6359478bfec76c772e7db0896533ed6d
Usuario <Usuario@CPD03-M05>
2024-03-18 11:24:04 -0300

    Fix no NovoProduto.xamls.cs

--- listaCompras/Views/NovoProduto.xaml.cs
+++ listaCompras/Views/NovoProduto.xaml.cs
@@ -11,18 +11,18 @@ public partial class NovoProduto : ContentPage
 
     private async void ToolbarItem_Clicked(object sender, EventArgs e)
     {
-		try
-		{
-			Produto p = new Produto();
-			{
-				Descricao = txt_descricao.Text,
+        try
+        {
+            Produto p = new Produto()
+            {
+                Descricao = txt_descricao.Text,
 				Quantidade = Convert.ToDouble(txt_quantidade.Text),
 				Preco = Convert.ToDouble(txt_preco.Text),
 			};
-		}
-		catch (Exception ex) 
-		{
-			await DisplayAlert("Ops", ex.Message, "OK");
-		}
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
